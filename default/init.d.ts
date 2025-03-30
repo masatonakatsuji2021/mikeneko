982f885ea96e80a1487060ee622ee13f8164c569
Nakatsuji Masato <nakatsuji.sak19@gmail.com>
2025-03-30 09:23:46 +0900

    Fixed a bug where transpiling would fail when building a project right after it was created.

--- default/init.d.ts
+++ default/init.d.ts
@@ -1,4 +1,5 @@
 // @ts-ignore
 // declare function require(path: string) : any;
+declare const platform : string;
 declare function use(path : string) : any;
 declare function useExists(path : string) : boolean;
